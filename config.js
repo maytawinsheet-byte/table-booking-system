cdee2ded9442affa83ccdb484feecebf5f4b3425
maytawinsheet-byte <maytawin.sheet@gmail.com>
2026-05-06 08:46:48 +0700

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// เปลี่ยน URL นี้เป็น Google Apps Script Web App URL ของคุณ
-const API_URL = 'https://script.google.com/macros/s/AKfycbx_oEKqkdd77RaMsLAxV6_cWAZDoC1WPjEfAWEIzQQ6mSzR2RdjgrK9O0VqJfiTh5cGug/exec';
-const STORE_ID = 'STORE001';
-const DEFAULT_LANG = 'th';
