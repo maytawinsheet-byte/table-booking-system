16d92c51beda8d91c298b7132ce03c6cc901d1e9
maytawinsheet-byte <maytawin.sheet@gmail.com>
2026-05-06 08:47:50 +0700

    Delete assets/js directory

--- assets/js/config.js
+++
@@ -1,3 +0,0 @@
-const API_URL = 'https://script.google.com/macros/s/AKfycbx_oEKqkdd77RaMsLAxV6_cWAZDoC1WPjEfAWEIzQQ6mSzR2RdjgrK9O0VqJfiTh5cGug/exec';
-const STORE_ID = 'STORE001';
-const DEFAULT_LANG = 'th';
