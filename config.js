5887d5d39c214ad437ab86254ae7c2cc377d3adb
maytawinsheet-byte <maytawin.sheet@gmail.com>
2026-05-06 08:31:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // เปลี่ยน URL นี้เป็น Google Apps Script Web App URL ของคุณ
-const API_URL = 'PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE';
+const API_URL = 'https://script.google.com/macros/s/AKfycbx_oEKqkdd77RaMsLAxV6_cWAZDoC1WPjEfAWEIzQQ6mSzR2RdjgrK9O0VqJfiTh5cGug/exec';
 const STORE_ID = 'STORE001';
 const DEFAULT_LANG = 'th';
